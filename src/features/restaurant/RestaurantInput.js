512feb71a9e245d504c7cdc0440914abf1927b57
John <johnfarris217@gmail.com>
2024-01-30 14:14:05 -0600

    commit tree 3690b7312a09cd9eb65b57fda7a3e2ec6f8ccb2e

--- src/features/restaurant/RestaurantInput.js
+++ src/features/restaurant/RestaurantInput.js
@@ -1,44 +1,47 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { addRestaurant } from "./restaurantsSlice";
 
 function RestaurantInput() {
-  const [name, setName] = useState("");
-  const [location, setLocation] = useState("");
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const dispatch = useDispatch();
 
-  function handleNameChange(event) {
-    setName(event.target.value);
-  }
+    function handleNameChange(event) {
+        setName(event.target.value);
+    }
 
-  function handleLocationChange(event) {
-    setLocation(event.target.value);
-  }
+    function handleLocationChange(event) {
+        setLocation(event.target.value);
+    }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    // add missing code
-  }
+    function handleSubmit(event) {
+        event.preventDefault();
+        // add missing code
+        dispatch(addRestaurant({ name: name, location: location }));
+    }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <p>
-        <input
-          type="text"
-          onChange={handleNameChange}
-          id="name"
-          placeholder="restaurant name"
-        />
-      </p>
-      <p>
-        <input
-          type="text"
-          onChange={handleLocationChange}
-          id="location"
-          placeholder="location"
-        />
-      </p>
-      <button type="submit">Submit</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <p>
+                <input
+                    type="text"
+                    onChange={handleNameChange}
+                    id="name"
+                    placeholder="restaurant name"
+                />
+            </p>
+            <p>
+                <input
+                    type="text"
+                    onChange={handleLocationChange}
+                    id="location"
+                    placeholder="location"
+                />
+            </p>
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
 
 export default RestaurantInput;
